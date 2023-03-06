aca44ece9212d89292ab182b3879185ce8f3f33e
chen <yifasmail@163.com>
2023-03-06 15:13:49 +0800

    add base project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    host: 'localhost',
+    title: '个人文档',
+    description: 'chen',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: '前端', link: '/blog/fronted/' },
+            { text: '后端', link: '/blog/backend/' },
+            { text: '导航', link: '/guide/guide' },
+        ]
+    }
+}
